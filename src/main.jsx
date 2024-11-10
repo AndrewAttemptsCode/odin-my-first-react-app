c040cd924734fa6e708ae5083b614104a7f4410b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-10 16:32:30 +0000

    Complete challenges

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import Greeting from './Greeting.jsx'
-import { FavFood } from './FavFood.jsx'
-import Bio from './challenge1.jsx'
-import TodoList from './challenge4.jsx'
+import DrinkList from './challenge7';
+
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <TodoList />
+  <DrinkList />
   </StrictMode>
 )
