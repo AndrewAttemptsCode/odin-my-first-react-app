a00ebc3dfd680336d3f50a88e62e50bfa5dc84ac
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-11 16:08:02 +0000

    Complete key challenge 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import List from './key/challenge_1/App';
+import RecipeList from './key/challenge_2/App';
 
 
 const rootElement = document.getElementById('root');
@@ -9,6 +9,6 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-  <List />
+  <RecipeList />
   </StrictMode>
 )
