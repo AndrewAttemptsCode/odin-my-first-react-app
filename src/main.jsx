d39cb99689cc3a19b69f40301a1c0f7d1fcd625a
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-13 18:20:50 +0000

    Complete state challenge_1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Profile from './passing-props/challenge_3/App';
+import Gallery from './state-memory/challenge_1/App';
 
 
 const rootElement = document.getElementById('root');
@@ -9,6 +9,6 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-  <Profile />
+  <Gallery />
   </StrictMode>
 )
