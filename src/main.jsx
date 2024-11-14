3e5538c4971164ee4e45ddc551665a6a53f8421d
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-14 13:56:00 +0000

    Complete state challenge_4

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import FeedbackForm from './state-memory/challenge_3/App';
+import FeedbackForm from './state-memory/challenge_4/App';
 
 
 const rootElement = document.getElementById('root');
