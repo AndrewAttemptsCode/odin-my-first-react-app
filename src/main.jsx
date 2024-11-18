05e9896092f287501fda3cb6a5ccbb62af1fb45b
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-11-18 13:34:07 +0000

    Complete state-between-components challenge_1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import MailClient from './state-structure/challenge_4/App';
+import SyncedInputs from './state-between-components/challenge_1/App';
 
 
 const rootElement = document.getElementById('root');
@@ -9,6 +9,6 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-  <MailClient />
+  <SyncedInputs />
   </StrictMode>
 )
