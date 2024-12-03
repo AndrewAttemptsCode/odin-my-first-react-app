da85524780c8b1cc19bdad33bb7a33cb916bca4f
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-03 13:51:01 +0000

    Complete challenge_2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './effect/challenge_1/App';
+import App from './effect/challenge_2/App';
 
 
 const rootElement = document.getElementById('root');
