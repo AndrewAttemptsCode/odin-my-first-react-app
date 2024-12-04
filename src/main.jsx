653fcf00c74521ae227273e2cf047d03a426dafd
AndrewAttemptsCode <161156094+AndrewAttemptsCode@users.noreply.github.com>
2024-12-04 21:59:49 +0000

    Complete challnege_3 of pt_2

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import TodoList from './effect/pt2/challenge_2/App';
+import ContactManager from './effect/pt2/challenge_3/App';
 
 
 const rootElement = document.getElementById('root');
@@ -9,6 +9,6 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-  <TodoList />
+  <ContactManager />
   </StrictMode>
 )
